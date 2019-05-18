3983c90f1b957ffa1e758f3407d31ca6113f8c59
amanchauhan11 <amanc2610@gmail.com>
2019-05-18 11:53:00 +0530

    writing controller

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import FileHandlers.FileHandlerImpl.CSVFileHandler;
+import FileHandlers.FileHandlerImpl.JsonFileHandler;
+import FileHandlers.FileHandlerImpl.XMLFileHandler;
+import FileHandlers.MyFileHandler;
+import model.MyCollection;
+import sun.jvm.hotspot.debugger.ThreadAccess;
+
 public class MyController {
+
+    public static void main(String[] args) {
+        MyFileHandler xmlHandler = new XMLFileHandler("employee.xml", "employee_out.xml");
+        MyFileHandler jsonHandler = new JsonFileHandler("employee.json", "employee_out.xml");
+        MyFileHandler csvHandler = new CSVFileHandler("employee.csv", "employee_out.csv");
+
+        ReadThread xmlRead = new ReadThread(xmlHandler);
+        ReadThread jsonRead = new ReadThread(jsonHandler);
+        ReadThread csvRead = new ReadThread(csvHandler);
+
+        xmlRead.start();
+        jsonRead.start();
+        csvRead.start();
+        xmlRead.join();
+        jsonRead.join();
+        csvRead.join();
+
+        MyCollection myCollection = MyCollection.getInstance();
+        System.out.println(myCollection.getWriteCounter());
+
+        
+
+    }
 }
