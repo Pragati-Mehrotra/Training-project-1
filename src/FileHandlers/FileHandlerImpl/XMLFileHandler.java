58f75d0e11e687e41829f27f98d27c12fea03ad1
pragatimehrotra <mehrotra.pragati0@gmail.com>
2019-05-18 13:06:40 +0530

    Read and Write function updated in XMLFileHandler.

--- src/FileHandlers/FileHandlerImpl/XMLFileHandler.java
+++ src/FileHandlers/FileHandlerImpl/XMLFileHandler.java
@@ -16,6 +16,11 @@ import org.xml.sax.SAXException;
 import java.text.SimpleDateFormat;
 import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.text.DateFormat;
 import java.text.ParseException;
 
@@ -28,12 +33,12 @@ public class XMLFileHandler implements MyFileHandler {
 
 
 
-    private int counter ;
+    private int readCounter ;
 
     XMLFileHandler(String path1,String path2){
         this.inputPath = path1;
         this.outputPath =path2;
-        counter =0;
+        readCounter =0;
         }
 
     @Override
@@ -49,8 +54,8 @@ public class XMLFileHandler implements MyFileHandler {
             doc.getDocumentElement().normalize();
 
             NodeList nList = doc.getElementsByTagName("employee");
-            emp = getEmployee(nList.item(counter));
-            counter++;
+            emp = getEmployeeRead(nList.item(readCounter));
+            readCounter++;
             return emp;
 
         }
@@ -63,7 +68,7 @@ public class XMLFileHandler implements MyFileHandler {
 
         }
 
-    private Employee getEmployee(Node node) {
+    private static Employee getEmployeeRead(Node node) {
         //XMLReaderDOM domReader = new XMLReaderDOM();
         Employee emp = new Employee();
         if (node.getNodeType() == Node.ELEMENT_NODE) {
@@ -89,7 +94,7 @@ public class XMLFileHandler implements MyFileHandler {
     }
 
 
-    private String getTagValue(String tag, Element element) {
+    private static String getTagValue(String tag, Element element) {
         NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
         Node node = (Node) nodeList.item(0);
         return node.getNodeValue();
@@ -98,6 +103,69 @@ public class XMLFileHandler implements MyFileHandler {
     @Override
     public void write(Employee emp) {
 
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
+            //add elements to Document
+            Element rootElement =
+                    doc.createElementNS("https://www.journaldev.com/employee", "Employees");
+            //append root element to document
+            doc.appendChild(rootElement);
+
+            //append first child element to root element
+            rootElement.appendChild(getWriteEmployee(doc, emp.getFirstName(), emp.getLastName(), emp.getDateOfBirth().toString(), emp.getExperience().toString()));
+
+            //for output to file, console
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            //for pretty print
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+
+            //write to console or file
+            StreamResult console = new StreamResult(System.out);
+            StreamResult file = new StreamResult(new File(outputPath));
+
+            //write data
+            transformer.transform(source, console);
+            transformer.transform(source, file);
+            System.out.println("DONE");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
     }
+
+    private static Node getWriteEmployee(Document doc,  String firstname, String lastname, String dateofbirth,
+                                         String experience) {
+        Element employee = doc.createElement("Employee");
+
+
+        employee.appendChild(getEmployeeElements(doc, employee, "firstname", firstname));
+
+
+        employee.appendChild(getEmployeeElements(doc, employee, "lastName", lastname));
+
+
+        employee.appendChild(getEmployeeElements(doc, employee, "dateOfBirth", dateofbirth));
+
+        employee.appendChild(getEmployeeElements(doc, employee, "experience", experience));
+
+        return employee;
+    }
+
+
+    //utility method to create text node
+    private static Node getEmployeeElements(Document doc, Element element, String name, String value) {
+        Element node = doc.createElement(name);
+        node.appendChild(doc.createTextNode(value));
+        return node;
+    }
+
 
 }
